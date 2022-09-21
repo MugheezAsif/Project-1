958d4640c117b2c3127a839fb363309313ba557f
MugheezAsif <m.mogheesasif@gmail.com>
2022-09-21 16:33:27 +0500

    added associations for accounts and posts

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  has_many :posts, dependent: :destroy
 end
